913b55a590052814a0ac839d22461fee368bd3b1
DorianDitilyeu <dorian.ditilyeu@laposte.net>
2020-06-09 11:54:34 +0200

    Fin de création du routeur + console.log

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules, Router } from '@angular/router';
 import { PageLoginComponent } from './login/pages/page-login/page-login.component';
 
 
 const routes: Routes = [
-  { path: "login", component: PageLoginComponent },
   { path: "", redirectTo: "/login", pathMatch: "full" },
   {
     path: "orders",
@@ -21,7 +20,15 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: false, //Debugging
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(router: Router) {
+    const replacer = (key, value) => (typeof value === "function") ? value.name : value;
+    console.log("Routes :", JSON.stringify(router.config, replacer, 2))
+  }
+ }
