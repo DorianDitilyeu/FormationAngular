2902a383a24358dcc43653f6cbebb3a6246908ee
DorianDitilyeu <dorian.ditilyeu@laposte.net>
2020-06-11 13:25:02 +0200

    Travail template -c

--- src/app/templates/templates.module.ts
+++ src/app/templates/templates.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import { TemplateAComponent } from './components/template-a/template-a.component';
 import { TemplateBComponent } from './components/template-b/template-b.component';
 import { TextModule } from '../text/text.module';
+import { TemplateCComponent } from './components/template-c/template-c.component';
 
 
 
 @NgModule({
-  declarations: [TemplateAComponent, TemplateBComponent],
+  declarations: [TemplateAComponent, TemplateBComponent, TemplateCComponent],
   imports: [
     CommonModule,
     TextModule
   ],
   exports: [
     TemplateAComponent,
-    TemplateBComponent
+    TemplateBComponent,
+    TemplateCComponent
   ]
 })
 export class TemplatesModule { }
